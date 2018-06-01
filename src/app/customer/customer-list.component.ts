50bd488903f5bb84906b45053290acd3f789ba17
Umesh Mandal <umesh.mandal619@gmail.com>
2018-06-01 11:43:51 +0545

    Create customer-list.component.ts

---
+++ src/app/customer/customer-list.component.ts
@@ -0,0 +1,54 @@
+import {
+    Component, OnInit, ComponentFactoryResolver, ViewChildren,
+    ViewContainerRef, QueryList, AfterViewInit, ViewChild, ComponentFactory
+} from '@angular/core';
+import { Router, ActivatedRoute } from "@angular/router";
+
+
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/range';
+
+import { filter, map, delay, delayWhen, flatMap } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
+import { merge } from 'rxjs/observable/merge';
+import { ADMIN_DB } from '../shared/mock-list';
+import { IEmployee } from '../shared/interface';
+import { EmployeeService } from '../shared/stepper.service';
+import { routerTransition } from '../router.animations';
+
+@Component({
+    selector: 'customer-list',
+    templateUrl: 'customer-list.component.html',
+    styles: [`
+    .a {
+        cursor: pointer;
+    }
+    .table-bordered {
+        border: 1px solid #d5dbe0;
+        color: snow;
+    }
+`],
+animations: [routerTransition()]
+
+})
+export class CustomerListComponent implements OnInit {
+
+    customerForm: FormGroup;
+    employees = {};
+
+    constructor(
+        public employeeService: EmployeeService,
+        private route: ActivatedRoute,
+        private router: Router,
+        private resolver: ComponentFactoryResolver,
+    ) {  }
+
+    ngOnInit() {
+        this.employeeService.getEmployees()
+            .subscribe(d => {
+                this.employees = d;
+            });
+    }
+
+}
