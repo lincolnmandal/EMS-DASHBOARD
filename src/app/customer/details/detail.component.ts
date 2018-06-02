d77e3724981bb2a7adc1298bbcdbc01221254f1e
Umesh Mandal <umesh.mandal619@gmail.com>
2018-06-02 17:48:49 +0545

    Add files via upload

--- src/app/customer/details/detail.component.ts
+++ src/app/customer/details/detail.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { EmployeeService } from '../../shared/stepper.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { IEmployee } from '../../shared/interface';
+import 'rxjs/add/operator/switchMap';
+import { routerTransition } from '../../router.animations';
 
 @Component({
     moduleId: module.id,
@@ -10,28 +12,41 @@ import { IEmployee } from '../../shared/interface';
 })
 
 export class DetailComponent implements OnInit {
-    sub: any;
+    // public hero: IEmployee;
+
+    // constructor(private _heroService: EmployeeService,
+    //   private _routeParams: ParamMap) {
+    // }
+
+    // ngOnInit() {
+    //   if (!this.hero) {
+    //     let id = +this._routeParams.get('id');
+    //     this._heroService.getHero(id).then(hero => this.hero = hero);
+    //   }
+    // }
+
     employees = [];
+    order: any;
     pageTitle: string = 'Customer Detail';
-    @Input() childData: IEmployee;
 
     constructor(
         private route: ActivatedRoute,
         private router: Router,
         private _employeeService: EmployeeService
-    ) { }
-    ngOnInit() {
-        let employees = this._employeeService.getEmployees();
-        let id = +this.route.snapshot.params['id'];
-        this.pageTitle += `: ${id}`;
-        // this.sub = this.route.params.subscribe(params => {
-            // let id = params['id'];
-            // Retrieve EmployeeDetails with Id route param
-            this._employeeService.getEmployee(id).subscribe(() => this.employees = this.employees);
-        // });
+    ) {
+        this._employeeService.getEmployees()
+            .subscribe(order => this.order = order);
     }
 
     onBack() {
-        this.router.navigate(['/customer']);
+        this.router.navigate(['customer']);
+    }
+
+    ngOnInit(): void {
+        let id = +this.route.snapshot.params['id'];
+        this.pageTitle += `: ${id}`;
+        this.route.paramMap
+            .switchMap((params: ParamMap) => this._employeeService.getEmployee(+params.get('id')))
+            .subscribe(order => this.order = order);
     }
 }
