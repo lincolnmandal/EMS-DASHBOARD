a48f337568cca392c0bc957954537c91fac6fa65
Umesh Mandal <umesh.mandal619@gmail.com>
2018-06-08 14:49:56 +0545

    Add files via upload

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, Validators, FormGroup, } from '@angular/forms';
 import { Router } from '@angular/router';
 import { trigger, transition, animate, style, state } from "@angular/animations";
 import { routerTransition } from '../router.animations';
 import { EmployeeService } from '../shared/stepper.service';
-
 
 @Component({
     templateUrl: 'register.component.html',
@@ -23,18 +22,22 @@ import { EmployeeService } from '../shared/stepper.service';
 })
 
 export class RegisterComponent implements OnInit {
+
     password: boolean = false;
     registerForm: FormGroup;
+    fileToUpload: File = null;
 
     constructor(
         private fb: FormBuilder,
         private router: Router,
         private adminService: EmployeeService,
-    ) {}
-    
+        private cd: ChangeDetectorRef
+    ) { }
+
     ngOnInit() {
         this.registerForm = this.fb.group({
             id: [0],
+            imageUrl: [null, Validators.required],
             firstName: [null, [Validators.required, Validators.minLength(3)]],
             lastName: [null, [Validators.required, Validators.minLength(3)]],
             email: [null, [Validators.required, Validators.email]],
@@ -49,6 +52,23 @@ export class RegisterComponent implements OnInit {
     togglePassword() {
         this.password = !this.password;
     }
+
+    imageUpload(event) {
+        const reader = new FileReader();
+        if (event.target.files && event.target.files.length) {
+            const [imageUrl] = event.target.files;
+            reader.readAsDataURL(imageUrl);
+
+            reader.onload = () => {
+                this.registerForm.patchValue({
+                    imageUrl: reader.result
+                });
+                // need to run CD since file load runs outside of zone
+                this.cd.markForCheck();
+            };
+        }
+    }
+
 
     register() {
         this.adminService.register(this.registerForm.value);
