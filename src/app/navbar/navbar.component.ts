5c3da596ace3221b5857809d185887ccddbd8eca
Umesh Mandal <umesh.mandal619@gmail.com>
2018-06-16 09:52:04 +0545

    Add files via upload

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
-import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { Location } from '@angular/common';
 import { AuthService } from '../shared/auth.service';
 
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
     isloginSuceeded: boolean = false;
